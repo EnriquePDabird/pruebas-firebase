732991258801a16a35a9d41219b9321ece0bfa22
EnriquePDabird <eph1806@gmail.com>
2026-02-25 12:45:24 +0000

    Eliminado importProviders

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,6 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-ae99c","appId":"1:1084361367184:web:519be87cb04cc56be04215","storageBucket":"friendlychat-ae99c.firebasestorage.app","apiKey":"AIzaSyAcLVFBq6_wkh4VajggLkM6_fRDvrhPiwM","authDomain":"friendlychat-ae99c.firebaseapp.com","messagingSenderId":"1084361367184","projectNumber":"1084361367184","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
